375ca53b03b357344e58e230a896c018c51fcf7d
Mark Vabulas <mark@trouvist.com>
2020-11-03 11:59:38 +0100

    Fixed HLSL not being included in the build
    HLSL was moved to a new folder using the #define, so we need to make sure we have the sources actually available.

--- cmake/3rdparty/glslang.cmake
+++ cmake/3rdparty/glslang.cmake
@@ -13,6 +13,7 @@ if( TARGET glslang )
 endif()
 
 file( GLOB GLSLANG_SOURCES
+	${BGFX_DIR}/3rdparty/glslang/glslang/HLSL/*.cpp
 	${BGFX_DIR}/3rdparty/glslang/glslang/GenericCodeGen/*.cpp
 	${BGFX_DIR}/3rdparty/glslang/glslang/MachineIndependent/*.cpp
 	${BGFX_DIR}/3rdparty/glslang/glslang/MachineIndependent/preprocessor/*.cpp
